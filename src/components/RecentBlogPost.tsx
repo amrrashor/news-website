d40178034c12150518bbc2626fd886228c3a99c2
amrashour <75447356+amrrashor@users.noreply.github.com>
2023-09-09 14:44:01 +0300

    added responsive layout for recent blog posts

--- src/components/RecentBlogPost.tsx
+++ src/components/RecentBlogPost.tsx
@@ -1,8 +1,6 @@
 import { Box, Flex, Grid, GridItem , Text} from "@chakra-ui/react";
 
 
-
-
 const RecentBlogPost = () => {
     return (
             <Box marginTop={'150px'}>
@@ -11,19 +9,20 @@ const RecentBlogPost = () => {
                 </Text>
             
                 <Flex justifyContent={'center'}  flexDirection={'column'} alignItems={'center'}>
-                <Grid
-                    gap={[0, 2, 5]}
-                    templateRows={['repeat(6, 1fr)', 'repeat(6, 1fr)', 'repeat(3, 267px)']}
-                    templateColumns={['repeat(1, 1fr)', 'repeat(2, 640px)', 'repeat(2, 640px)']}
-                >
-                        <GridItem colSpan={1} rowSpan={[1,2,2]} maxW={'1280px'} bg='red' minH={'267px'}>Box 1</GridItem>
-                        <GridItem colSpan={1} rowSpan={1} bg='black' maxW={'1280px'}  h={'267px'}>Box 2</GridItem>
-                        <GridItem colSpan={1} rowSpan={1} bg='green' maxW={'1280px'}  h={'267px'}>Box 3</GridItem>
-                        <GridItem colSpan={2} bg='cyan' maxW={'1280px'} w='100%' h={'267px'}>Box 4</GridItem>
+                    <Grid
+                        maxW={'1280px'}
+                        columnGap={[1,1,1]}
+                        rowGap={[1,1,1]}
+                        templateRows={['repeat(4, 480px)', 'repeat(6, 1fr)', 'repeat(3, 267px)']}
+                        templateColumns={['repeat(1, auto)', 'repeat(2, 640px)', 'repeat(2, 640px)']}
+                    >
+                    <GridItem colSpan={1} rowSpan={[1, 2, 2]} w={['328px', '100%']}  maxW={'1280px'} bg='red' minH={['480px','267px']}>Box 1</GridItem>
+                        <GridItem colSpan={1} rowSpan={[1,1,1]} bg='black'  w={['328px', '100%']} maxW={'1280px'}  minH={['480px','267px']}>Box 2</GridItem>
+                        <GridItem colSpan={1} rowSpan={[1,1,1]} bg='green'  w={['328px', '100%']} maxW={'1280px'}  minH={['480px','267px']}>Box 3</GridItem>
+                        <GridItem colSpan={[1,2,2]} rowSpan={[1]} bg='cyan'   w={['328px', '100%']}maxW={'1280px'} w='100%' minH={['480px','267px']}>Box 4</GridItem>
                     </Grid>
                 </Flex>
             </Box>
-            
     )
 }
 
