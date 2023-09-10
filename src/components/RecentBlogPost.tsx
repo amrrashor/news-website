82845da2185f1f5d2421763b6070fde5a00f5855
amrashour <75447356+amrrashor@users.noreply.github.com>
2023-09-10 16:30:00 +0300

    added style sheet

--- src/components/RecentBlogPost.tsx
+++ src/components/RecentBlogPost.tsx
@@ -1,6 +1,11 @@
-import { Box, Flex, Grid, GridItem, Text, Image } from "@chakra-ui/react";
-import { useQuery} from 'react-query';
+import { Box, Flex, Grid, GridItem, Text, Image, Spinner  } from "@chakra-ui/react";
+import { useQuery } from 'react-query';
+
+//PROP TYPES
 import { rescentBlogPostProps } from "@/utils/types";
+//STYLES
+import { RecentBlogsStyles } from "@/utils/styles";
+
 
 const SinglePost = ({id, userId, title, body}: rescentBlogPostProps) => {
 return (
@@ -10,30 +15,32 @@ return (
         w={['328px', '100%']}
         maxW={'100%'}
         minH={['480px', '267px']}
-        boxShadow={'2xl'}
-        bg={'white'}
         key={id}
     >
         <Flex flexDirection={id === 1 ? 'column' : 'row' }>
             <Image
-                src={''}
+                src={'https://ichef.bbci.co.uk/news/976/cpsprodpb/16646/production/_116681719_whatsubject.jpg'}
                 alt={title}
                 maxW={'100%'}
                 w={id === 1 ? '100%' : id === 4 ? '592px' : '328px'}
                 maxH={'228px'}
                 objectFit={'cover'}
+                marginBottom={'8px'}
             />
 
-            <Box>
+            <Box marginLeft={'10px'}>
                 <Flex>
-                    {/* <Text>{authorName}</Text> */}
-                    {/* <Text>{date}</Text> */}
+                    <Text sx={RecentBlogsStyles.Date_Author} marginRight={'5px'}>{title.slice(0, 8)}</Text>
+                    <Text sx={RecentBlogsStyles.Date_Author} marginLeft={'5px'}>28/12/2022</Text>
                 </Flex>
-                <Text>{title}</Text>
-                <Text>{body}</Text>
-                {/* {tags.map((tag) => (
-                    <Text key={tag}>{tag}</Text>
-                ))} */}
+                <Text sx={RecentBlogsStyles.Title}>{title.slice(0, 20)}</Text>
+                <Text sx={RecentBlogsStyles.Author} marginRight={'5px'}>{title.slice(0, 8)}</Text>
+                <Text sx={RecentBlogsStyles.Body}>{body}</Text>
+                <Flex>
+                    <Text sx={RecentBlogsStyles.Tags}>{title.slice(0, 4)}</Text>
+                    <Text sx={RecentBlogsStyles.Tags}>{title.slice(0, 3)}</Text>
+                    <Text sx={RecentBlogsStyles.Tags}>{title.slice(0, 2)}</Text>
+                </Flex>
             </Box>
         </Flex>
     </GridItem>
@@ -47,28 +54,28 @@ const RecentBlogPost = () => {
         )
     )
 
-    if (isLoading) return 'Loading...'
-
-    if (error) return 'An error has occurred: ' + error
-    console.log('data', data)
     return (
         <Box marginLeft={'auto'} marginRight={'auto'} maxW={'1280px'} marginTop={'150px'}>
-            <Text as={'h3'} textAlign={'left'} marginBottom={'40px'}>
+            <Text as={'h3'} sx={RecentBlogsStyles.Section_Header}>
                 Recent blog posts
             </Text>
 
             <Flex justifyContent={'center'} flexDirection={'column'} alignItems={'center'}>
+                {isLoading ? (
+                    <Spinner />
+                ): (
                     <Grid
                         maxW={'1280px'}
-                        columnGap={[1, 1, 2]}
-                        rowGap={[1, 1, 2]}
+                        columnGap={[1, 1, 3]}
+                        rowGap={[1, 1, 3]}
                         templateRows={['repeat(4, 480px)', 'repeat(6, 1fr)', 'repeat(3, 267px)']}
                         templateColumns={['repeat(1, auto)', 'repeat(2, 640px)', 'repeat(2, 640px)']}
                     >
-                    {data.slice(0,4).map((post: string) => (
-                        <SinglePost {...post} />
+                    {data.slice(0,4).map((post: rescentBlogPostProps) => (
+                        <SinglePost {...post}/>
                     ))}
                 </Grid>
+                )}
             </Flex>
         </Box>
     )
---
+++ src/utils/styles.ts
@@ -0,0 +1,51 @@
+//RECENT BLOG POSTS STYLES
+export const RecentBlogsStyles = {
+    Section_Header: {
+        fontSize: '24px',
+        lineHeight: "32px",
+        fontWeight: "600",
+        color: "#1A1A1A",
+        marginBottom: '40px',
+        textAling:'left',
+    },
+    Date_Author: {
+        textColor: '#6941C6',
+        fontSize: '14px',
+        lineHeight: '16px',
+        fontWeight:'600'
+    },
+    Title: {
+        fontSize: '24px',
+        fontWeight: 600,
+        lineHeight: '32px',
+        color: "#1A1A1A",
+        marginTop:'5px'
+    },
+    Author: {
+        fontSize: '14px',
+        lineHeight: '20px',
+        color: '#21D188',
+        fontWeight:'600'
+    },
+    Body: {
+        fontsize: '16px',
+        lineHeight: '24px',
+        fontWeight: '500',
+        color:'#667085',
+    },
+    Tags: {
+        fontSize: '14px',
+        lineHeight: '20px',
+        fontWeight: '500',
+        color: '#6941C6',
+        marginRight: '10px',
+        bgGradient: 'linear(to-l, #e0dde7, #F9F5FF)',
+        rounded: '5px',
+        textAlign: 'center',
+        px: '2px',
+        marginTop: '8px',
+        width:'50px'
+    }
+}
+
+
